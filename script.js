f8c72a583ae125209e89a83290de92a8f56df78a
Helton Rios <helton.placido@gmail.com>
2021-10-12 14:40:27 -0300

    html e css do site

---
+++ script.js
@@ -0,0 +1,5 @@
+// Carne - 400gr por pessoa + de 6 horas - 650
+// Cerveja - 1200ml por pessoa + de 6 horas - 2000ml
+// Refrigerante/agua - 1000ml por pessoa + de 6 horas - 1500ml
+
+// crianças valem 0,5
